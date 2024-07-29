c9f10bd8d8ccd21e1ed8ca79d21bb67244826fca
vasu <vasuinecjn@gmail.com>
2024-07-29 11:24:04 +0530

    fixing the build issue

--- bin.sh
+++ bin.sh
@@ -1,7 +1,7 @@
 # shellcheck disable=SC2164
-export python='/opt/homebrew/bin/python3'
-export pytest='/Library/Frameworks/Python.framework/Versions/3.12/bin/pytest'
-export pip='/opt/homebrew/bin/pip3'
+python3 -m venv selenium_env
+. selenium_env/bin/activate
+pip3 install -r requirements.txt
 
 rm -rf reports
 rm -rf screenshots
@@ -9,3 +9,14 @@ mkdir screenshots
 cd src/tests/
 echo `pwd`
 pytest test_login.py -sv -n 2 --browser chrome --html=../../reports/report.html
+
+
+#python3 -m venv selenium_env
+#. selenium_env/bin/activate
+#pip3 install -r requirements.txt
+#rm -rf reports
+#rm -rf screenshots
+#mkdir screenshots
+#cd src/tests/
+#echo `pwd`
+#pytest test_login.py -sv -n 2 --browser chrome --html=../../reports/report.html
