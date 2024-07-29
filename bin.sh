c202e06ab064f1b71cb26ec45896b268dab094f4
vasu <vasuinecjn@gmail.com>
2024-07-29 11:01:01 +0530

    fixing the issue

--- bin.sh
+++ bin.sh
@@ -1,9 +1,7 @@
 # shellcheck disable=SC2164
-export python=/opt/homebrew/bin/python3
-export pip=/opt/homebrew/bin/pip3
-pip install -r requirements.txt
 rm -rf reports
 rm -rf screenshots
 mkdir screenshots
 cd src/tests/
+echo `pwd`
 pytest test_login.py -sv -n 2 --browser chrome --html=../../reports/report.html
